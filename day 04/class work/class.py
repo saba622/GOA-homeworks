7ec7cecc44d6b0d152b8ee6527b6332a02777cf8
saba622 <saba.samashvilisaba@gmail.com>
2025-02-23 22:12:06 +0400

    second commit

---
+++ day 04/class work/class.py
@@ -0,0 +1,12 @@
+level = 0
+print(level)
+
+level = level +1
+print(level)
+
+level = level +2
+print(level)
+
+level = level +3
+print(level)
+
---
+++ day 04/home work/home.py
@@ -0,0 +1,8 @@
+fullname = "saba samashvili"
+about = "me davdivar cekvaze"
+
+print (fullname)
+
+print(about)
+
+print(5+5)
